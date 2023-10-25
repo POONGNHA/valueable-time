6484f5313b8cf7deb4a922e041c64964ae425f49
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2023-10-25 21:28:20 +0900

    try make react app

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export const Timer = memo(() => {
-    const minutes_in_ms = 3 * 60 * 1000;
-    const interval = 1000;
-    const [timeLeft, setTimeLeft] = useState<number>(minutes_in_ms);
+// export const Timer = memo(() => {
+//     const minutes_in_ms = 3 * 60 * 1000;
+//     const interval = 1000;
+//     const [timeLeft, setTimeLeft] = useState<number>(minutes_in_ms);
 
-    const minutes = String(Math.floor((timeLeft / (1000 * 60)) % 60)).padStart(2, '0');
-    const second = String(Math.floor((timeLeft / 1000) % 60)).padStart(2,'0');
+//     const minutes = String(Math.floor((timeLeft / (1000 * 60)) % 60)).padStart(2, '0');
+//     const second = String(Math.floor((timeLeft / 1000) % 60)).padStart(2,'0');
 
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTimeLeft((prevTime) => prevTime - interval);
-        },interval);
+//     useEffect(() => {
+//         const timer = setInterval(() => {
+//             setTimeLeft((prevTime) => prevTime - interval);
+//         },interval);
     
-            if (timeLeft <= 0){
-                clearInterval(timer);
-                console.log("타이머 종료");
-            }
+//             if (timeLeft <= 0){
+//                 clearInterval(timer);
+//                 console.log("타이머 종료");
+//             }
             
-            return () => {
-                clearInterval(timer);
-            };
-        }, [timeLeft]);
+//             return () => {
+//                 clearInterval(timer);
+//             };
+//         }, [timeLeft]);
 
 
-        return (
-            <div>
-                {minutes} : {second}
-            </div>
-        )
+//         return (
+//             <div>
+//                 {minutes} : {second}
+//             </div>
+//         )
     
 
-});
+// });
---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+
+
+import {Profile} from '@components/study/Profile';
+
+
+
+
+const App = () => {
+    return (
+        <div className='App'>
+            <Profile name='name' job='job' />
+            {/* <Counter />
+            <TodoList /> */}
+        </div>
+    );
+};
+
+export default App;
